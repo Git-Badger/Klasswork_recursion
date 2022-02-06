4b49a4fbd15b72b5bcd3d8723d12b186f800020a
Alfia Saifieva <gitbadger@gmail.com>
2022-02-06 17:33:56 +0300

    Числа показаны

---
+++ 1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Написать рекурсивную функцию показа чисел от 1 до N. N задано");
+
+int number;
+
+Console.WriteLine("Введите число: ");
+number = int.Parse(Console.ReadLine());
+
+void countBackward(int N)
+{
+    if (N >= 1)
+    {
+        Console.WriteLine(N);
+        countBackward(N - 1);
+    }
+}
+
+void countForward(int start, int end)
+{
+    if (end >= start)
+    {
+        Console.WriteLine(start);
+        countForward(start + 1, end);
+    }
+}
+
+Console.WriteLine($"Посчитаем от {number} до единицы");
+countBackward(number);
+Console.WriteLine($"Посчитаем от единицы до {number}");
+countForward(1, number);
+
