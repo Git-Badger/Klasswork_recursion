4cbd23a567693bee4b458840a113fbae75cd3fb3
Alfia Saifieva <gitbadger@gmail.com>
2022-02-06 16:53:03 +0300

    Сумма чисел посчитана

---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Рекурсивная f для определения суммы цифр числа N, введеного с клавиатуры");
+
+int number;
+
+Console.WriteLine("Введите число ");
+number = int.Parse(Console.ReadLine());
+
+int count(int N, int summ)
+{
+    if (N < 10) return N;
+    summ = summ + count(N / 10, summ);
+    return summ + N % 10;
+}
+
+Console.WriteLine($"Сумма цифр: {count(number, 0)}");
